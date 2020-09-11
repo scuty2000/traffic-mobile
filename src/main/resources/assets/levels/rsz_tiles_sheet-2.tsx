9e797c508f6c887a2e1dc4e0d0799c8630a32a10
Sancheitti-1883210 <sanchietti.1883210@studenti.uniroa1.it>
2020-09-11 04:17:58 +0200

    added a map

---
+++ src/main/resources/assets/levels/rsz_tiles_sheet-2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="rsz_tiles_sheet" tilewidth="125" tileheight="125" tilecount="42" columns="7">
+ <image source="../textures/rsz_tiles_sheet.png" width="875" height="750"/>
+</tileset>
